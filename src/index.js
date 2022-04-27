8656a41469ce00b5dee12e456409978c6fbfb60a
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-04-28 00:10:15 +0300

    refactor: split classes into different modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './index.css';
 
+import Options from './Options';
+import Month from './Month';
+
 /*
 Return calendar HTML
     Options:
@@ -12,126 +15,6 @@ Return calendar HTML
     Return function f(year, month)
 */
 
-const defaultOptions = {
-    type: 'year',
-    monthNames: [
-        'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
-        'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-    weekDaysNames: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-    weekendDays: ['Сб', 'Вс'],
-    firstWeekday: 'Пн'
-};
-
-class Options {
-    constructor(options) {
-        options = options || {};
-        this.type = options.type === 'month' ? options.type : defaultOptions.type;
-        this.monthNames = options.monthNames || defaultOptions.monthNames;
-        this.weekDaysNames = options.weekDaysNames || defaultOptions.weekDaysNames;
-        this.weekendDays = options.weekendDays || defaultOptions.weekendDays;
-        this.firstWeekday = options.firstWeekday || defaultOptions.firstWeekday;
-        this.firstWeekdayIndex = this.getFirstWeekdayIndex();
-        this.visibleWeekDays = this.getVisibleWeekDays();
-    }
-
-    getFirstWeekdayIndex() {
-        return this.weekDaysNames.indexOf(this.firstWeekday) > -1 ? this.weekDaysNames.indexOf(this.firstWeekday) : 0;
-    }
-
-    getVisibleWeekDays() {
-        return [
-            ...this.weekDaysNames.slice(this.firstWeekdayIndex),
-            ...this.weekDaysNames.slice(0, this.firstWeekdayIndex)
-        ];
-    }
-}
-
-class Month {
-    constructor(year, month, visibleWeekDays, options) {
-        this.visibleWeekDays = visibleWeekDays;
-        this.options = new Options(options);
-        this.setYear(year);
-        this.setMonth(month);
-        this.caption = this.getMonthCaption();
-        this.daysCount = this.getDaysCount();
-        this.beginningEmptyCells = this.getBeginningEmptyCells();
-        this.endEmptyCells = this.getEndEmptyCells(this.daysCount, this.beginningEmptyCells);
-        this.days = this.getDays();
-    }
-
-    setYear(year) {
-        if (typeof year === 'number' && !isNaN(year) && year >= 0) {
-            this.year = year;
-        } else {
-            this.year = new Date().getFullYear();
-        }
-    }
-
-    setMonth(month) {
-        if (typeof month === 'string') {
-            month = this.options.monthNames.indexOf(month);
-        }
-        if (typeof month === 'number') {
-            month = Math.trunc(month);
-        }
-        if (typeof month !== 'number' || isNaN(month) || month < 0 || month >= 12) {
-            month = new Date().getMonth();
-        }
-        this.month = month;
-    }
-
-    getMonthCaption() {
-        return this.options.monthNames[this.month]
-    }
-
-    getDaysCount() {
-        const date = new Date();
-        date.setDate(1);
-        date.setYear(this.year);
-        date.setMonth(this.month + 1);
-        date.setDate(0);
-        return date.getDate();
-    }
-
-    getBeginningEmptyCells() {
-        const firstDayOfWeek = this.getFirstDayOfWeek();
-        const beginningEmptyCells = this.options.visibleWeekDays.indexOf(firstDayOfWeek);
-        if (beginningEmptyCells === -1) {
-            beginningEmptyCells = 0;
-        }
-        return beginningEmptyCells;
-    }
-
-    getEndEmptyCells(daysCount, beginningEmptyCells) {
-        const totalCells = this.options.visibleWeekDays.length * 6;
-        return totalCells - daysCount - beginningEmptyCells;
-    }
-
-    getFirstDayOfWeek() {
-        const date = new Date();
-        date.setDate(1);
-        date.setYear(this.year);
-        date.setMonth(this.month);
-        return this.options.weekDaysNames[date.getDay()];
-    }
-
-    getDays() {
-        return [
-            ...emptyValues(this.beginningEmptyCells),
-            ...days(this.daysCount),
-            ...emptyValues(this.endEmptyCells)
-        ];
-    }
-
-    getLayout() {
-        return this.days.reduce(layoutBuilder, []);
-    }
-}
-
-const emptyValues = (count) => new Array(count).fill('&nbsp');
-
-const days = (count) => [...sequence(1, count)];
-
 function * sequence(from, to) {
     for (let i = from; i <= to; i++) yield i;
 }
@@ -143,19 +26,6 @@ layout is two dimentional array 7 column 6 row
     ...
 ]
 */
-
-function layoutBuilder(layout, value) {
-    if (!Array.isArray(layout)) {
-        layout = layoutBuilder([], layout);
-    }
-    layout =
-        (!layout.length || layout[layout.length - 1].length === 7) ?
-        [...layout, []] :
-        [...layout];
-    layout[layout.length - 1] = [...layout[layout.length - 1], value];
-    return layout;
-}
-
 
 function calendar(options) {
     options = new Options(options);
