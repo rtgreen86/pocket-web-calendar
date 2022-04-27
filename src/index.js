7b85c56f3af281f7ffdda8a60338a4646b4577fd
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-04-27 23:46:42 +0300

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 /*
 Return calendar HTML
     Options:
@@ -235,3 +237,18 @@ export default class Calendar {
         this.element.innerHTML = curriedCalendar(this.year, this.month);
     }
 }
+
+const month = document.createElement('DIV');
+month.classList.add('container', 'calendar');
+document.body.appendChild(month);
+const myCalendar1 = new Calendar(month, { type: 'month' });
+myCalendar1.year = 1986;
+myCalendar1.month = 'Февраль';
+myCalendar1.show();
+
+const year = document.createElement('DIV');
+year.classList.add('container', 'calendar');
+document.body.appendChild(year);
+const myCalendar2 = new Calendar(year, { });
+myCalendar2.year = 1986;
+myCalendar2.show();
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
