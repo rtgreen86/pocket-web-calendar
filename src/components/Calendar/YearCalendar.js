0c2ff5932479efcbde1698b4150f6d97a88ee100
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2022-06-13 10:14:40 +0300

    feat: show year caption

--- src/components/Calendar/YearCalendar.js
+++ src/components/Calendar/YearCalendar.js
@@ -6,7 +6,9 @@ import MonthGrid from './MonthGrid';
 import { YearCalendarPropTypes } from './PropTypes';
 
 export default function YearCalendar({year, firstDayOfWeek, weekendDays}) {
-  return (<div className="container calendar"><div className="year">{
+  return (<div className="container calendar">
+    <div className="caption">{year}</div>
+    <div className="year">{
     Array.from({length: 12}, (el, idx) => idx).map((month) => (
       <div key={month} className="month">
         <MonthGrid year={year} month={month} firstDayOfWeek={firstDayOfWeek} weekendDays={weekendDays}/>
